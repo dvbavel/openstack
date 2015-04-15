eca4e49529957ae476c38e0e264335636b87964b
Dyon van Bavel <dyon@dyon.co>
2015-04-15 14:42:20 +0200

    Corrected view names

--- openstack-dashboard/enterprisesupport/supportlinks/urls.py
+++ openstack-dashboard/enterprisesupport/supportlinks/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns
 from django.conf.urls import url
 
 from openstack_dashboard.dashboards.enterprisesupport.supportlinks.views \
-    import IndexView
+    import supportlinksview
 
 
 urlpatterns = patterns(
     '',
-    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^$', supportlinksview.as_view(), name='index'),
 )
