e197c98f6f2eabb4a2bcd0a77c2467f22f1cc4f8
Dyon van Bavel <dyon@dyon.co>
2015-04-15 12:43:11 +0200

    adjusted urls.py

--- openstack-dashboard/enterprisesupport/supportoverview/urls.py
+++ openstack-dashboard/enterprisesupport/supportoverview/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns
 from django.conf.urls import url
 
-from openstack_dashboard.dashboards.enterprisesupport.supportoverview.views \
-    import IndexView
+from openstack_dashboard.dashboards.enterprisesupport.supportoverview import views
 
 
-urlpatterns = patterns(
-    '',
-    url(r'^$', IndexView.as_view(), name='index'),
+urlpatterns = patterns('',
+    url(r'^$',
+        views.IndexView.as_view(), name='index'),
 )
