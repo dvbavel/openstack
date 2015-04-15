c662c797a77ee9b5f684da02bfdee6d7d21c9c91
Dyon van Bavel <dyon@dyon.co>
2015-04-15 12:28:30 +0200

    Added group class

--- openstack-dashboard/enterprisesupport/dashboard.py
+++ openstack-dashboard/enterprisesupport/dashboard.py
@@ -3,11 +3,16 @@ from django.utils.translation import ugettext_lazy as _
 import horizon
 
 
+class enterprisesupportgroup(horizon.PanelGroup):
+    slug = "enterprisesupportgroup"
+    name = _("Enterprise Support Group")
+    panels = ('supportoverview',)
+
 class Enterprisesupport(horizon.Dashboard):
     name = _("Support Center")
     slug = "enterprisesupport"
-    panels = ()  # Add your panels here.
-    default_panel = ''  # Specify the slug of the dashboard's default panel.
+    panels = (enterprisesupportgroup,)  # Add your panels here.
+    default_panel = 'supportoverview'  # Specify the slug of the dashboard's default panel.
 
 
 horizon.register(Enterprisesupport)
