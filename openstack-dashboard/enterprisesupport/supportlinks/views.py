4ab6bd8a3a494d52354c6e39da2607da986620fb
Dyon van Bavel <dyon@dyon.co>
2015-04-15 14:49:56 +0200

    Corrected import modules

--- openstack-dashboard/enterprisesupport/supportlinks/views.py
+++ openstack-dashboard/enterprisesupport/supportlinks/views.py
@@ -1,4 +1,4 @@
-from horizon import views
+from django.views.generic.base import TemplateView
 
 class supportlinksview(TemplateView):
     template_name = 'enterprisesupport/supportlinks/index.html'
